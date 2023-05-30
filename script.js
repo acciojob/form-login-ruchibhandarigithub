9ced3566d806c5c10b1a1c6513ad71efde14adca
Ruchi Bhandari <98799210+ruchibhandarigithub@users.noreply.github.com>
2023-05-30 23:14:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function getFormvalue() {
-	let Name = document.getElementsByTagName('input');
-    alert(Name[0]+" "+Name[1]);
+	  var form = document.getElementById("form1");
+  
+  // Get the values of the first name and last name fields
+  var firstName = form.fname.value;
+  var lastName = form.lname.value;
+  
+  // Create the alert message
+  var alertMessage =  firstName +" "+ lastName;
+  
+  // Display the alert message
+  alert(alertMessage);
+
 
 }
